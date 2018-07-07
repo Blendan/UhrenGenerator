b14ce2ddf9a0e04a2df7fa83ca6f474ce104f153
Blendan <37484137+Blendan@users.noreply.github.com>
2018-07-07 15:52:42 +0200

    out put als file faste fertig

--- resources/app/js/out.js
+++ resources/app/js/out.js
@@ -1,3 +1,6 @@
+var app = require('electron').remote;
+dialog = app.dialog;
+
 function gernireOutput()
 {
   if (zeigeranzBS)
@@ -9,10 +12,10 @@ function gernireOutput()
   }
 
 
-  var markanzeigen_var = "";
+  var markanzeigen_var1 = "";
   if(markanzeigen)
   {
-    markanzeigen_var ='.zwischenstriche {'+'width: 100%;'+'height: 100%;'+'background: none;'+'position: absolute;'+'}';
+    markanzeigen_var1 ='.zwischenstriche {'+'width: 100%;'+'height: 100%;'+'background: none;'+'position: absolute;'+'}';
   }
 
   if (markBSanzeigen)
@@ -34,10 +37,10 @@ function gernireOutput()
   }
 
 
-  var markanzeigen_var = "";
+  var markanzeigen_var2 = "";
   if (markanzeigen)
   {
-    markanzeigen_var ='<div class="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<divclass="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>';
+    markanzeigen_var2 ='<div class="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<divclass="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_gross striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>'+'<div class="zwischenstriche"><div class="strich_klein striche"></div></div>';
   }
 
   var zeigeranz_var = "";
@@ -75,234 +78,254 @@ function gernireOutput()
     '}';
   }
 
-  var out = '<!DOCTYPE html><html lang="de" dir="ltr"><head><meta charset="utf-8"><title>Uhr</title><style>'+
-      'html, body {'+
-        'width: 100%;'+
-        'height: 100%;'+
-        'margin: 0;'+
-        'padding: 0;'+
-        'transition: all 0.3s;'+
-        'transition-timing-function: linear;'+
-        'overflow: hidden;'+
-        'background : '+document.getElementById("color_hintergrund").value+';'+
-      '}'+
-      '#Uhr {'+
-        'position: relative;'+
-        'top: '+document.getElementById("abstand_oben").value+'px;'+
-        'margin-left: auto;'+
-        'margin-right: auto;'+
-        'width: '+document.getElementById("size").value+'px;'+
-        'height: '+document.getElementById("size").value+'px;'+
-        'box-shadow: 0 0 50px '+document.getElementById("color_schatten").value+';'+
-        'border: 1px solid '+document.getElementById("color_schatten").value+';'+
-        'border-radius: '+document.getElementById("rundung").value+'px;'+
-        'color : '+document.getElementById("color_zeiger").value+';'+
-        'background: none;'+
-        'transition: all 1s;'+
-        'transition-timing-function: ease-in-out;'+
-      '}'+
+  var markanzeigen_var3 = " ";
+  if (markanzeigen)
+  {
+    markanzeigen_var3 = 'var array = document.getElementsByClassName("zwischenstriche");'+
+    'var grad = 0;'+
+    'for (var i = 0; i < array.length; i++)'+
+    '{'+
+      'array[i].style.transform = "rotate("+grad+"deg)";'+
+      'grad = grad + 30;'+
+    '}';
+  }
 
-  '<div class="Generator_zeiger">'+
-      '#Uhr #kreis{'+
-        'position: absolute;'+
-        'top: 50%;'+
-        'left: 50%;'+
-        'width: 20px;'+
-        'height: 20px;'+
-        'margin-left: -10px;'+
-        'margin-top: -10px;'+
-        'background: '+document.getElementById("color_zeiger").value+';'+
-        'border: 1px solid black;'+
-        'border-radius: 100%;'+
-      '}'+
-
-      '#Uhr .zeiger {'+
-        'position: absolute;'+
-        'top: 50%;'+
-        'left: 50%;'+
-        'width: 10px;'+
-        'height: 10px;'+
-        'margin-left: -5px;'+
-        'margin-top: -5px;'+
-        'background: '+document.getElementById("color_zeiger2").value+';'+
-        'box-shadow: 0 0 '+  zeigeranzBS_var+'px '+document.getElementById("color_zeigerBS").value+';'+
-      '}'+
-
-
-      '#Uhr #h_zeiger{'+
-        'border-top: '+document.getElementById("grosse_zeiger").value/2+'px solid '+document.getElementById("color_zeiger").value+';'+
-      '}'+
-
-      '#Uhr #m_zeiger{'+
-        'border-top: '+document.getElementById("grosse_zeiger").value/1.5+'px solid '+document.getElementById("color_zeiger").value+';'+
-      '}'+
-
-      '#Uhr #s_zeiger{'+
-        'border-top: '+document.getElementById("grosse_zeiger").value+'px solid '+document.getElementById("color_zeiger").value+';'+
-      '}'+
-
-      '.harkig {'+
-        'transition: all 1s;'+
-        'transition-timing-function: linear;'+
-      '}'+
-
-      '#Uhr .zeigerfeld {'+
-        'width: 100%;'+
-        'height: 100%;'+
-        'position: absolute;'+
-      '}'+
-  '</div>'+
-  +markanzeigen_var+
-    '.zwischenstriche .strich_gross {'+
-      'position: absolute;'+
-      'top: 50%;'+
-      'left: 50%;'+
-      'width: 10px;'+
-      'height: 30px;'+
-      'margin-left: -5px;'+
+  var puls_an_var = "";
+  if (puls_an)
+  {
+    puls_an_var = 'if (puls_gros) {'+
+      'document.getElementById("Uhr").style.boxShadow = "0 0 10px";'+
+      'puls_gros = false;'+
     '}'+
+    'else {'+
+      'document.getElementById("Uhr").style.boxShadow = "0 0 100px";'+
+      'puls_gros = true;'+
+    '}';
+  }
 
-    '.zwischenstriche .strich_klein {'+
-      'position: absolute;'+
-      'top: 50%;'+
-      'left: 50%;'+
-      'width: 5px;'+
-      'height: 20px;'+
-      'margin-left: -2.5px;'+
-    '}'+
+  var dreidanz_var = "";
+  if (dreidanz)
+  {
+    dreidanz_var = 'onmousemove="dreiD(event)"';
+  }
 
-    '.striche {'+
-      'margin-top: '+document.getElementById("radius_mark").value+';'+
-      'background: '+document.getElementById("color_mark").value+';'+
-      'box-shadow: 0 0   '+markBSanzeigen_var+'px '+document.getElementById("color_BSmark").value+';'+
-      'transition: all 0.5s;'+
-    '}'+
-  '</div>'+
+  var out = '<!DOCTYPE html><html lang="de" dir="ltr">\n'+
+  '<head><meta charset="utf-8"><title>Uhr</title><style>\n'+
+      'html, body {\n'+
+        'width: 100%;\n'+
+        'height: 100%;\n'+
+        'margin: 0;\n'+
+        'padding: 0;\n'+
+        'transition: all 0.3s;\n'+
+        'transition-timing-function: linear;\n'+
+        'overflow: hidden;\n'+
+        'background : '+document.getElementById("color_hintergrund").value+';\n'+
+      '}\n'+
+      '#Uhr {\n'+
+        'position: relative;\n'+
+        'top: '+document.getElementById("abstand_oben").value+'px;\n'+
+        'margin-left: auto;\n'+
+        'margin-right: auto;\n'+
+        'width: '+document.getElementById("size").value+'px;\n'+
+        'height: '+document.getElementById("size").value+'px;\n'+
+        'box-shadow: 0 0 50px '+document.getElementById("color_schatten").value+';\n'+
+        'border: 1px solid '+document.getElementById("color_schatten").value+';\n'+
+        'border-radius: '+document.getElementById("rundung").value+'px;\n'+
+        'color : '+document.getElementById("color_zeiger").value+';\n'+
+        'background: none;\n'+
+        'transition: all 1s;\n'+
+        'transition-timing-function: ease-in-out;\n'+
+      '}\n'+
+
+      '#Uhr #kreis{\n'+
+        'position: absolute;\n'+
+        'top: 50%;\n'+
+        'left: 50%;\n'+
+        'width: 20px;\n'+
+        'height: 20px;\n'+
+        'margin-left: -10px;\n'+
+        'margin-top: -10px;\n'+
+        'background: '+document.getElementById("color_zeiger").value+';\n'+
+        'border: 1px solid black;\n'+
+        'border-radius: 100%;\n'+
+      '}\n'+
+
+      '#Uhr .zeiger {\n'+
+        'position: absolute;\n'+
+        'top: 50%;\n'+
+        'left: 50%;\n'+
+        'width: 10px;\n'+
+        'height: 10px;\n'+
+        'margin-left: -5px;\n'+
+        'margin-top: -5px;\n'+
+        'background: '+document.getElementById("color_zeiger2").value+';\n'+
+        'box-shadow: 0 0 '+  zeigeranzBS_var+'px '+document.getElementById("color_zeigerBS").value+';\n'+
+      '}\n'+
+
+
+      '#Uhr #h_zeiger{\n'+
+        'border-top: '+document.getElementById("grosse_zeiger").value/2+'px solid '+document.getElementById("color_zeiger").value+';\n'+
+      '}\n'+
+
+      '#Uhr #m_zeiger{\n'+
+        'border-top: '+document.getElementById("grosse_zeiger").value/1.5+'px solid '+document.getElementById("color_zeiger").value+';\n'+
+      '}\n'+
+
+      '#Uhr #s_zeiger{\n'+
+        'border-top: '+document.getElementById("grosse_zeiger").value+'px solid '+document.getElementById("color_zeiger").value+';\n'+
+      '}\n'+
+
+      '.harkig {\n'+
+        'transition: all 1s;\n'+
+        'transition-timing-function: linear;\n'+
+      '}\n'+
+
+      '#Uhr .zeigerfeld {\n'+
+        'width: 100%;\n'+
+        'height: 100%;\n'+
+        'position: absolute;\n'+
+      '}\n'+
+
+    +markanzeigen_var1+
+    '\n.zwischenstriche .strich_gross {\n'+
+      'position: absolute;\n'+
+      'top: 50%;\n'+
+      'left: 50%;\n'+
+      'width: 10px;\n'+
+      'height: 30px;\n'+
+      'margin-left: -5px;\n'+
+    '}\n'+
+
+    '.zwischenstriche .strich_klein {\n'+
+      'position: absolute;\n'+
+      'top: 50%;\n'+
+      'left: 50%;\n'+
+      'width: 5px;\n'+
+      'height: 20px;\n'+
+      'margin-left: -2.5px;\n'+
+    '}\n'+
+
+    '.striche {\n'+
+      'margin-top: '+document.getElementById("radius_mark").value+';\n'+
+      'background: '+document.getElementById("color_mark").value+';\n'+
+      'box-shadow: 0 0   '+markBSanzeigen_var+'px '+document.getElementById("color_BSmark").value+';\n'+
+      'transition: all 0.5s;\n'+
+    '}\n'+
 
   textangezeigt_var+
 
-      '#liveanzeige {'+
-        'transition: all 0.2s;'+
-        'width: 100%;'+
-        'height: 100%;'+
-      '}'+
-      '</style>'+
-    '</head>'+
-    '<body onmousemove="dreiD(event)">'+
-    '<div id="liveanzeige">'+
-      '<div id="Uhr">'+
-        markanzeigen_var+
+      '#liveanzeige {\n'+
+        'transition: all 0.2s;\n'+
+        'width: 100%;\n'+
+        'height: 100%;\n'+
+      '}\n'+
+      '</style>\n'+
+    '</head>\n'+
+    '<body \n'+dreidanz_var+'>\n'+
+    '<div id="liveanzeige">\n'+
+      '<div id="Uhr">\n'+
+        markanzeigen_var2+
       zeigeranz_var+
-      '</div>'+
-      '<div class="Generator_zahlen"><div id="digital">'+
-        '<div id="zeit"></div>'+
-      '</div></div>'+
-    '</div>'+
-    '<script>'+
-      '"use strict";'+
-      '<div class="Generator_puls">var puls_gros = false;</div>'+
+      '<div class="Generator_zahlen"><div id="digital">\n'+
+        '<div id="zeit"></div>\n'+
+      '</div></div>\n'+
+    '</div>\n'+
+    '<script>\n'+
+      '"use strict";\n'+
+      'var puls_gros = false;\n'+
 
-      'starten();'+
+      'starten();\n'+
 
-      'function starten() {'+
-        '<div class="Generator_Mark">'+
-          'var array = document.getElementsByClassName("zwischenstriche");'+
-          'var grad = 0;'+
+      'function starten() {\n'+
+        +markanzeigen_var3+'\n'+
+        'setInterval(zeitAnzeigen , 1000);\n'+
+      '}\n'+
 
-          'for (var i = 0; i < array.length; i++)'+
-          '{'+
-            'array[i].style.transform = "rotate("+grad+"deg)";'+
-            'grad = grad + 30;'+
-          '}'+
+      'function zeitAnzeigen() {\n'+
 
-        '</div>'+
-        'setInterval(zeitAnzeigen , 1000);'+
-      '}'+
+        puls_an_var+
 
-      '<div class="Generator_puls">function pulsMachen() {'+
-          'if (puls_gros) {'+
-            'document.getElementById("Uhr").style.boxShadow = "0 0 10px";'+
-            'puls_gros = false;'+
-          '}'+
-          'else {'+
-            'document.getElementById("Uhr").style.boxShadow = "0 0 100px";'+
-            'puls_gros = true;'+
-          '}'+
-        '}</div>'+
+        'var zeit = new Date();\n'+
+        'var h = zeit.getHours();\n'+
+        'var m = zeit.getMinutes();\n'+
+        'var s = zeit.getSeconds();\n'+
 
-      'function zeitAnzeigen() {'+
+        'var hdeg = h*30+180;\n'+
+        'var mdeg = m*6+180;\n'+
+        'var sdeg = s*6+180;\n'+
 
-        '<div class="Generator_puls">pulsMachen();</div>'+
+        'document.getElementById("h").style.transform = "rotate("+hdeg+"deg)";\n'+
+        'document.getElementById("m").style.transform = "rotate("+mdeg+"deg)";\n'+
+        'document.getElementById("s").style.transform = "rotate("+sdeg+"deg)";\n'+
 
-        'var zeit = new Date();'+
-        'var h = zeit.getHours();'+
-        'var m = zeit.getMinutes();'+
-        'var s = zeit.getSeconds();'+
+        'if(s < 10)\n'+
+        '{\n'+
+          's ="0"+ s;\n'+
+        '}\n'+
 
-        '<div class="Generator_zeiger">var hdeg = h*30+180;'+
-        'var mdeg = m*6+180;'+
-        'var sdeg = s*6+180;'+
+        'if(m < 10)\n'+
+        '{\n'+
+          'm ="0"+ m;\n'+
+        '}\n'+
 
-        'document.getElementById("h").style.transform = "rotate("+hdeg+"deg)";'+
-        'document.getElementById("m").style.transform = "rotate("+mdeg+"deg)";'+
-        'document.getElementById("s").style.transform = "rotate("+sdeg+"deg)";'+
-      '</div>'+
-        '<div class="Generator_zahlen">if(s < 10)'+
-        '{'+
-          's ="0"+ s;'+
-        '}'+
+        'document.getElementById("zeit").innerHTML = h + ":" + m + ":" + s;\n'+
+      '}\n'+
+        'function dreiD(event) {\n'+
+            'var x = event.clientX;\n'+
+            'var y = event.clientY;\n'+
+            'var x_g = window.screen.availWidth;\n'+
+            'var y_g = window.screen.availHeight;\n'+
 
-        'if(m < 10)'+
-        '{'+
-          'm ="0"+ m;'+
-        '}'+
+            'x /= x_g;\n'+
+            'y /= y_g;\n'+
 
-        'document.getElementById("zeit").innerHTML = h + ":" + m + ":" + s;</div>'+
-      '}'+
-      '<div class="Generator_3d">'+
-        'function dreiD(event) {'+
-            'var x = event.clientX;'+
-            'var y = event.clientY;'+
-            'var x_g = window.screen.availWidth;'+
-            'var y_g = window.screen.availHeight;'+
+            'x = Math.round(x*100, 1);\n'+
+            'y = Math.round(y*100, 1);\n'+
+            'x /= 100;\n'+
+            'y /= 100;\n'+
 
-            'x /= x_g;'+
-            'y /= y_g;'+
+            'if(x < 0.55 && x > 0.45)\n'+
+            '{\n'+
+              'x = 0;\n'+
+            '}\n'+
+            'else if(x > 0.5)\n'+
+            '{\n'+
+              'x *= -1;\n'+
+            '}\n'+
+            'else {\n'+
+              'x *= 2;\n'+
+            '}\n'+
 
-            'x = Math.round(x*100, 1);'+
-            'y = Math.round(y*100, 1);'+
-            'x /= 100;'+
-            'y /= 100;'+
+            'if(y < 0.55 && y > 0.45)\n'+
+            '{\n'+
+              'y = 0;\n'+
+            '}\n'+
+            'else if(y > 0.5)\n'+
+            '{\n'+
+              'y *= -1;\n'+
+            '}\n'+
+            'else {\n'+
+              'y *= 2;\n'+
+            '}\n'+
+            'y *= -1;\n'+
+            'document.getElementById("liveanzeige").style.transform = "rotate3d("+y+", "+x+", 0, '+document.getElementById("staerke_3d").value+')"\n'+
+        '}\n'+
+    '</script>\n'+
 
-            'if(x < 0.55 && x > 0.45)'+
-            '{'+
-              'x = 0;'+
-            '}'+
-            'else if(x > 0.5)'+
-            '{'+
-              'x *= -1;'+
-            '}'+
-            'else {'+
-              'x *= 2;'+
-            '}'+
+    '</body>\n'+
+  '</html>';
 
-            'if(y < 0.55 && y > 0.45)'+
-            '{'+
-              'y = 0;'+
-            '}'+
-            'else if(y > 0.5)'+
-            '{'+
-              'y *= -1;'+
-            '}'+
-            'else {'+
-              'y *= 2;'+
-            '}'+
-            'y *= -1;'+
-            'document.getElementById("liveanzeige").style.transform = "rotate3d("+y+", "+x+", 0, <div class="float_left" id="starke3d_gen"></div>'+
-        '}'+
-      '</div>'+
-    '</script>'+
+  return out;
+}
 
-    '</body>'+
-  '</html>'
+function saveFile()
+{
+  // fileName is a string that contains the path and filename created in the save file dialog.
+  fs.writeFile("Clock.html", gernireOutput(), (err) => {
+      if(err){
+          alert("An error ocurred creating the file "+ err.message)
+      }
+
+      alert("The file has been succesfully saved");
+  });
 }
--- resources/app/main.js
+++ resources/app/main.js
@@ -1,17 +1,19 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow()
-  mainWindow.maximize()
+  mainWindow = new BrowserWindow();
+  mainWindow.maximize();
+
+  mainWindow.openDevTools();
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
   mainWindow.setMenu(null);
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
